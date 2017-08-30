a829190e33efeeca08cbc4414756fcbe80a63b08
Christian Wick <christian@recmanplugins.no>
2017-08-30 04:59:47 +0200

    1.29.10.56

--- app/scripts/eventPage.js
+++ app/scripts/eventPage.js
@@ -1,5 +1,5 @@
 // Enable chromereload by uncommenting this line:
-import 'chromereload/devonly';
+// import 'chromereload/devonly';
 // if (ENV === 'development') {
 // 	console.log('We are in development mode!');
 // }
